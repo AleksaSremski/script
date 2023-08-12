0979c349f499a6768ebf7220663c4b1d773f2831
AleksaSremski <aleksa.03mrdja@gmail.com>
2023-08-12 17:00:11 +0200

    Novo

--- script.sh
+++ script.sh
@@ -1,12 +1,12 @@
 #!/bin/sh
 # Skripta za skidanje odredjenih programa i mojih konfiguracionih file-ova
 
-#	Variables
+###   Variables   ###
 dotfiles="https://github.com/AleksaSremski/dotfiles.git"
 Ipkgs=(curl git ntp base-devel)
 export TERM=ansi
 
-#	Functions
+###   Functions   ###
 installpackage() {
 	pacman --noconfirm --needed -S "$1" >/dev/null 2>&1
 }
@@ -51,7 +51,8 @@ welcomemsg() {
 	}
  }
 
- preinstallmsg() {
+preinstallmsg() {
+# Ask if you want to proceed with installation or exit
 	whiptail --title "Lets get started!" --yes-button "Yes, I agree." \
 		--no-button "No, exit script." \
 		--yesno "From this point whole script is goint to be automated, so if you want to exit now is the time.\nIf you want to continue script will create new user and install my 'DE'." 15 70 || {
@@ -73,7 +74,7 @@ getuserandpass() {
 # Get passwords
 	pass1=$(whiptail --title "Lets add password to the new user" --passwordbox "Please type your password:" 10 60 3>&1 1>&2 2>&3 3>&1) || exit 1
 	pass2=$(whiptail --title "Lets add password to the new user" --passwordbox "Please retype your password:" 10 60 3>&1 1>&2 2>&3 3>&1) || exit 1
-	# In case passwords do not match
+# In case passwords do not match
 	while ! [ $pass1 = $pass2 ]; do
 		pass1=$(whiptail --title "Passwords do not match!" --passwordbox "Please type again your password:" 10 60 3>&1 1>&2 2>&3 3>&1) || exit 1
 		pass2=$(whiptail  --passwordbox "Please retype your password:" 10 60 3>&1 1>&2 2>&3 3>&1) || exit 1
@@ -81,6 +82,7 @@ getuserandpass() {
 }
 
 usercheck() {
+# Check if user already exists on the system and prompts user if he wants to proceed
 	! { id -u "$username" >/dev/null 2>&1; } ||
 		whiptail --title "WARNING" --yes-button "CONTINUE" \
 			--no-button "No wait..." \
@@ -88,8 +90,9 @@ usercheck() {
 }
 
 adduserandpass() {
+# Adds user
 	whiptail  --infobox "User '$username' is being created" 7 50
-	useradd -m -g wheel -s /bin/bash "$username" || #>/dev/null/ 2>&1 ||
+	useradd -m -g wheel "$username" || #>/dev/null/ 2>&1 ||
 		usermod -a -G wheel "$username" && mkdir -p /home/"$username" && chown "$username":wheel /home/"$username"
 	echo "$username:$pass1" | chpasswd
 	unset	pass1 pass2
@@ -104,17 +107,18 @@ yayinstall() {
 	cd /home/"$username"/.config
 	sudo -u "$username" git clone https://aur.archlinux.org/"$1".git >/dev/null 2>&1
 	cd "$1"
-	sudo -u "$username" -D /home/"$username"/.config/"$1" \
-		makepkg --noconfirm -si >/dev/null 2>&1 || return 1
+	sudo -u "$username" makepkg --noconfirm -si >/dev/null 2>&1 || return 1
 }
 
 installpkg() {
+# Installing packages with pacman
 	whiptail --title "Installation!" \
 	--infobox "Program \"$program\" is being installed. ($n:$total)\n$program: $desc." 15 70
 	installpackage "$program"
 }
 
 maininstall() {
+# Main installation loop
 	n=0
 	total=$(wc -l </tmp/programs)
 	while read -r tag program desc; do
@@ -130,7 +134,7 @@ maininstall() {
 }
 
 gitinstall() {
-#	Installation via git, used only to install st, dwm, dmenu and slstatus!
+# Installation via git, used only to install st, dwm, dmenu and slstatus!
 	whiptail --title "Installation!" \
 	--infobox "Program \"$program\" is being installed. ($n:$total)\n$program: $desc." 15 70
 	cd "/home/"$username"/.config/"
@@ -142,20 +146,22 @@ gitinstall() {
 }
 
 deploydotfiles() {
+# Deploys my configurational files
 	whiptail --title "Configuration of programs!" \
 	--infobox "Deploying my configurational files." 15 70
 	cd "/tmp"
 	git clone "$1" >/dev/null 2>&1
-	mv dotfiles/.config/* /home/"$username"/.config/ && rm -rf dotfiles/.config/
+	cp -r dotfiles/.config/* /home/"$username"/.config/ && rm -rf dotfiles/.config/
 	cp dotfiles/* /home/"$username"/ && chown -R "$username":wheel /home/"$username"/*
 }
 
 finalize() {
+# Final message
 	whiptail --title "Installation complete!" --msgbox "Installation has finished, you can login as new user.\n\nEnjoy!" 14 70
 	clear
 }
 
-###	START	 ###
+###   START   ###
 # Pacman sync
 pacmansync
 
@@ -186,6 +192,7 @@ sed -i "s/-j2/-j$(nproc)/;/^#MAKEFLAGS/s/^#//" /etc/makepkg.conf
 # Installs yay (which is needed to install other packages)
 yayinstall "yay-bin"
 
+# Synchronize time
 whiptail --title "A few important things!" \
 	--infobox "Synchronizing system time to ensure successful and secure installation of software..." 8 70
 ntpd -q -g >/dev/null 2>&1
@@ -198,40 +205,3 @@ deploydotfiles "$dotfiles"
 # Final Message
 finalize
 
-
-
-# Installs package archlinux-keyring
-#	refresh_keys
-
-# yayinstall "librewolf-bin" #|| error "User exited"
-# echo "#####################################################"
-# echo "		" "PACKAGE IS INSTALLED"
-# echo "#####################################################"
-# removepackage "librewolf-bin" #|| error "User exited"
-
-# Package installation loop
-# for pkgs in ${packages[@]}; do
-# 	whiptail --title "Installation!" \
-# 		--infobox "Package '$pkgs'  is installing." 7 50
-# 	installpackage "$pkgs"
-# done
-#
-# for pkgs in ${packages[@]}; do
-# 	removepackage "$pkgs"
-# done
-
-#	whiptail --title "Installing package via yay" --yes-button "Yes, I agree" \
-#	--no-button "No, Fuck you!" \
-#	--yesno "DO YOU AGREE TO INSTALL '$1' PACKAGE?!" 15 60 || {
-#		clear
-#		exit 1
-#	}
-
-#	whiptail --title "Installation!" \
-#	--infobox "Program \"$program\" is being installed. ($n:$total)\n$program: $desc." 15 70
-#	#installpackage "$program"
-#	pacman -Si "$program" | head -4 >> testic
-#	echo "" >> testic
-
-#packages=(xorg-server xorg-xinit xorg-xrandr noto-fonts ttf-linux-libertine ttf-font-awesome xf86-video-intel libxft libxinerama git feh ffmpeg sxiv xwallpaper xcompmgr firefox mpd ncmpcpp mpc)
-#packages=(lf pass)
